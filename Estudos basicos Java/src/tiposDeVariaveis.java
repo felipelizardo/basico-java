f26c1f353f78980fa77c151acd49866be98b9ea6
felipelizardo <106035259+felipelizardo@users.noreply.github.com>
2023-08-31 14:43:28 -0300

    estudos de variaveis

--- Estudos basicos Java/src/tiposDeVariaveis.java
+++ Estudos basicos Java/src/tiposDeVariaveis.java
@@ -1,5 +1,21 @@
-public class App {
+public class tiposDeVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short)numeroNormal;
+
+
+
+        int num1 = 1;
+        
+        num1 = 10;
+
+        System.out.print(numero)
+
+        final double VALOR_PI = 3.14;
+
     }
 }
