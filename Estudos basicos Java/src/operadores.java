81a841bbb0cfb1f9caeba5bf2e888f0f2fce233c
felipelizardo <106035259+felipelizardo@users.noreply.github.com>
2023-09-01 19:35:43 -0300

    estudos

---
+++ Estudos basicos Java/src/operadores.java
@@ -0,0 +1,39 @@
+public class  operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "linguagem" + "java"
+
+        String concatenacao = "?"
+
+        String concatenacao = 1+1+1+"1"
+
+        System.out.printIn(concatenacao)
+
+        String concatenacao = 1+"1+"+1+1
+
+        System.out.printIn(concatenacao)
+
+        String concatenacao = 1+"1"+1+"1"
+
+        System.out.printIn(concatenacao)
+
+        String concatenacao = "1"+1+1+1
+
+        System.out.printIn(concatenacao)
+
+        String concatenacao = "1"+(1+1+1)
+
+    }
+
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.printIn(numero);
+
+        numero = numero * -1;
+
+        System.out.printIn(numero);
+    }
+}
+
